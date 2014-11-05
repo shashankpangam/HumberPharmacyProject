7eeb224cfebdb2d8ef2dace3bdd5e9faad2397bc
sharethevision <j.chaudhary@live.com>
2014-11-05 01:57:54 -0500

    added sidebar.php

---
+++ View/sidebar.php
@@ -0,0 +1,26 @@
+<div id="sidebar">
+    <div id="navigation">
+        <ul>
+            <li><a href="sideNavLinks.php">Medicines</a></li>
+            <li><a href="sideNavLinks.php">Vitamins</a></li>
+            <li><a href="sideNavLinks.php">Diet and Fitness</a></li>
+            <li><a href="sideNavLinks.php">Personal</a></li>
+            <li><a href="productSale.php">Featured Products</a></li>
+            <li><a href="index.php">Checkout</a></li>
+        </ul>
+        <div id="cart">
+            <strong>Shopping cart:</strong> <br /> 0 items
+        </div>
+    </div>
+    <div>
+        <img src="../images/title2.gif" alt="" width="233" height="41" /><br />																																																																																																																																																															
+        <div class="review">
+            <a href="#"><img src="../images/pic1.jpg" alt="" width="181" height="161" /></a>
+            <br />
+            <a href="login.php">Product 07</a><br />
+            <p>Dolor sit amet, consetetur sadipscing elitr, seddiam nonumy eirmod tempor. invidunt ut labore et dolore magna </p>
+            <img src="../images/stars.jpg" alt="" width="118" height="20" class="stars" />
+
+        </div>
+    </div>
+</div>
