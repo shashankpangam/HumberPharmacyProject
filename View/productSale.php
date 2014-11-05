df5b18e9315c62bd897356d939b26aed96a9e054
sharethevision <j.chaudhary@live.com>
2014-11-05 01:56:50 -0500

    added productSale.php

---
+++ View/productSale.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'header.php';
+?>
+<div id="content">
+    <?php require_once './sidebar.php'; ?>
+    <div id="main">
+        <div id="inside">
+            <div class="saleprodheader">
+                <img src="../images/title4.gif" alt="" width="159" height="17" /><br />
+            </div>
+            <div id="items">
+                <div class="item">
+                    <a href="#"><img src="../images/pic1.jpg" width="213" height="192" /></a><br />
+                    <p><a href="login.php">Product 01</a></p><span class="price">$125</span><br />
+                </div>
+                <div class="item center">
+                    <a href="#"><img src="../images/pic1.jpg" width="213" height="192" /></a><br />
+                    <p><a href="login.php">Product 02</a></p><span class="price">$215</span><br />
+                </div>
+                <div class="item">
+                    <a href="#"><img src="../images/pic1.jpg" width="213" height="192" /></a><br />
+                    <p><a href="login.php">Product 03</a></p><span class="price">$85</span><br />
+                </div>
+                <div class="item">
+                    <a href="#"><img src="../images/pic1.jpg" width="213" height="192" /></a><br />
+                    <p><a href="login.php">Product 04</a></p><span class="price">$35</span><br />
+                </div>
+                <div class="item center">
+                    <a href="#"><img src="../images/pic1.jpg" width="213" height="192" /></a><br />
+                    <p><a href="login.php">Product 05</a></p><span class="price">$27</span><br />
+                </div>
+                <div class="item">
+                    <a href="#"><img src="../images/pic1.jpg" width="213" height="192" /></a><br />
+                    <p><a href="login.php">Product 06</a></p><span class="price">$40</span><br />
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<?php require_once './footer.php'; ?>
