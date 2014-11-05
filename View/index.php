486f12977e9dfebc3ec61020137138bde0e62f17
sharethevision <j.chaudhary@live.com>
2014-11-05 01:56:08 -0500

    edit index.php

--- View/index.php
+++ View/index.php
@@ -1,6 +1,40 @@
-<?php 
+<?php
 require_once 'header.php';
-require_once 'content.php';
+?>
+<?php
+require_once '../Model/Databases.php';
+?>
+
+<div id="content">
+
+    <?php require_once './sidebar.php'; ?>
+
+    <div id="main">
+        <div class="container">
+            <div id="slides">
+                <img src="../images/slider1image.jpg" alt="" width="650" height="374" border="0">
+                <img src="../images/slider2image.jpg" alt="" width="650" height="374" border="0">
+                <img src="../images/slider3image.jpg" alt="" width="650" height="374" border="0">
+            </div>
+        </div>
+        <br />
+        <div id="inside">
+            <!-- OUR SHOP ICON-->
+            <img src="../images/title3.gif" alt="" width="159" height="15" /><br />
+            <div class="info">
+                <!-- FEMALE PICTURE-->
+                <img src="../images/pic2.jpg" alt="" width="159" height="132" />
+                <p>Dolor sit amet, consetetur sadipscing elitr, seddiam nonumy eirmod tempor. invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadip- scing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur. </p>
+                <p>Dolor sit amet, consetetur sadipscing elitr, seddiam nonumy eirmod tempor. invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadip- scing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur. </p>
+                <!-- MORE INFO ICON-->
+                <a href="contactus.php" class="more"><img src="../images/more.gif" alt="" width="106" height="28" /></a> 					
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php
 require_once 'footer.php';
 ?>
 
