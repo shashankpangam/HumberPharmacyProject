9e42245a83014a7dc01eca5635885c949072062e
ashish0106 <ashish.d0106@gmail.com>
2014-11-18 13:42:52 -0500

    Revert "commit"
    This reverts commit 3240035c9c1cdbedafb9ed4a0c68be463343dc9e.
    
    Conflicts:
    	Model/ShoppingCart.php
    	View/viewCart.php

--- View/viewCart.php
+++ View/viewCart.php
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 <?php
 require_once './header.php';
 require_once '../Model/Product_DB.php';
@@ -81,3 +82,23 @@ require_once './footer.php';
 ?>
 
 >>>>>>> origin/master
+=======
+<?php
+require_once './header.php';
+require_once '../Model/Product_DB.php';
+$current_url = base64_encode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+session_start();
+?>
+<div id="content">
+    <?php require_once './sidebar.php'; ?>
+    <div id="main">
+        <div id="inside">
+            
+        </div>
+    </div>
+</div>
+<?php
+require_once './footer.php';
+?>
+
+>>>>>>> parent of 3240035... commit
