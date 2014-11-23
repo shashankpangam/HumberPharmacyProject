cc7dfd6aacb218f7bf4f73f433e03d2e3de26b83
Shashank Pangam <shashankpangam@gmail.com>
2014-11-23 15:43:24 -0500

    Contact Us Fixed

--- View/contactus.php
+++ View/contactus.php
@@ -1,81 +1,94 @@
 <?php
 require_once 'Mail.php';
-if (isset($_POST['submit'])) {
-    $to = 'vortepharmacy@gmail.com'; //vorte pharmay email
-    $from = $_POST['email']; // user email 
-    $subject = $_POST['name']; // user name
-    $body = $_POST['comment']; //user comments
-    $is_body_html = true;
-
-    try {
-        send_email($to, $from, $subject, $body, $is_body_html);
-    } catch (Exception $ex) {
-        $error = $ex->getMessage();
-        #echo $error;
-    }
-}
-    function send_email($to, $from, $subject, $body, $is_body_html = false) {
-
-        $smtp = array();
-        // **** You must change the following to match your
-        // **** SMTP server and account information.
-        $smtp['host'] = 'ssl://smtp.gmail.com';
-        $smtp['port'] = 465;
-        $smtp['auth'] = true;
-        $smtp['username'] = 'vortepharmacy@gmail.com';
-        $smtp['password'] = 'hello919hello';
-
-        $mailer = Mail::factory('smtp', $smtp);
-        if (PEAR::isError($mailer)) {
-            throw new Exception('could not create mailer');
-        }
-
-        // Add the email address to the list of all recipients
-        $recipients = array();
-        $recipients[] = $to;
-
-        // Set the headers
-        $headers = array();
-        $headers['From'] = $from;
-        $headers['To'] = $to;
-        $headers['Subject'] = $subject;
-        if ($is_body_html) {
-            $headers['Content-type'] = 'text/html';
-        }
-
-        // Send the email
-        $result = $mailer->send($recipients, $headers, $body);
-
-        // Check the result and throw an error if one exists
-        if (PEAR::isError($result)) {
-            throw new Exception('error sending email: ' . htmlspecialchars($result->getMessage()));
-        }
-        return $result;
-    }
-
-
+//if (isset($_POST['submit'])) {
+//    
+//}
 // define variables and set to empty values
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $comment = $website = "";
+//if(isset($_POST["submit"]))
+//{
+//    $flag=true;
+//    if(empty($_POST["name"]))
+//    {
+//        $nameErr = "*Name is required";
+//        $flag = false;
+//    }
+//    else if (isset($_POST["name"]))
+//    {
+//        $name = test_input($_POST["name"]);
+//        if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+//            $nameErr = "*Only letters and white space allowed";
+//            $flag = false;
+//        }
+//    }
+//    else if (empty($_POST["email"])) {
+//        $emailErr = "*Email is required";
+//        $flag = false;
+//    }
+//    else if(isset($_POST["email"]))
+//    {
+//        $email = test_input($_POST["email"]);
+//        // check if e-mail address is well-formed
+//        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//            $emailErr = "*Invalid email format";
+//            $flag = false;
+//        }
+//    }
+//    else if(isset($_POST["comment"]))
+//    {
+//        $comment = test_input($_POST["comment"]);
+//    }
+//    else if(empty($_POST("gender")))
+//    {
+//        $genderErr = "*Gender is required";
+//        $flag = false;
+//    }
+//    else if(isset($_POST["gender"]))
+//    {
+//        $gender = test_input($_POST["gender"]);
+//    }
+// else {
+//        $to = 'vortepharmacy@gmail.com'; //vorte pharmay email
+//        $from = $_POST['email']; // user email 
+//        $subject = $_POST['name']; // user name
+//        $body = $_POST['comment']; //user comments
+//        $is_body_html = true;
+//
+//        try {
+//            send_email($to, $from, $subject, $body, $is_body_html);
+//        } catch (Exception $ex) {
+//            $error = $ex->getMessage();
+//            #echo $error;
+//        }
+//        header('location: thankyou.php?name=' . $name);
+//    }
+//}
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+if (isset($_POST["submit"])) {
+    $flag = true;
     if (empty($_POST["name"])) {
         $nameErr = "*Name is required";
+        $flag = false;
     } else {
         $name = test_input($_POST["name"]);
         // check if name only contains letters and whitespace
         if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
             $nameErr = "*Only letters and white space allowed";
+            $flag = false;
         }
     }
 
     if (empty($_POST["email"])) {
         $emailErr = "*Email is required";
+        $flag = false;
     } else {
         $email = test_input($_POST["email"]);
         // check if e-mail address is well-formed
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $emailErr = "*Invalid email format";
+            $flag = false;
         }
     }
 
@@ -87,14 +100,66 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (empty($_POST["gender"])) {
         $genderErr = "*Gender is required";
+        $flag = false;
     } else {
         $gender = test_input($_POST["gender"]);
     }
-    
 
-    header('location: thankyou.php?name='.$name);
-    
-    
+    if ($flag==true) {
+        $to = 'vortepharmacy@gmail.com'; //vorte pharmay email
+        $from = $_POST['email']; // user email 
+        $subject = $_POST['name']; // user name
+        $body = $_POST['comment']; //user comments
+        $is_body_html = true;
+
+        try {
+            send_email($to, $from, $subject, $body, $is_body_html);
+        } catch (Exception $ex) {
+            $error = $ex->getMessage();
+            #echo $error;
+        }
+        header('location: thankyou.php?name=' . $name);
+        $_POST=null;
+    }
+}
+
+function send_email($to, $from, $subject, $body, $is_body_html = false) {
+
+    $smtp = array();
+    // **** You must change the following to match your
+    // **** SMTP server and account information.
+    $smtp['host'] = 'ssl://smtp.gmail.com';
+    $smtp['port'] = 465;
+    $smtp['auth'] = true;
+    $smtp['username'] = 'vortepharmacy@gmail.com';
+    $smtp['password'] = 'hello919hello';
+
+    $mailer = Mail::factory('smtp', $smtp);
+    if (PEAR::isError($mailer)) {
+        throw new Exception('could not create mailer');
+    }
+
+    // Add the email address to the list of all recipients
+    $recipients = array();
+    $recipients[] = $to;
+
+    // Set the headers
+    $headers = array();
+    $headers['From'] = $from;
+    $headers['To'] = $to;
+    $headers['Subject'] = $subject;
+    if ($is_body_html) {
+        $headers['Content-type'] = 'text/html';
+    }
+
+    // Send the email
+    $result = $mailer->send($recipients, $headers, $body);
+
+    // Check the result and throw an error if one exists
+    if (PEAR::isError($result)) {
+        throw new Exception('error sending email: ' . htmlspecialchars($result->getMessage()));
+    }
+    return $result;
 }
 
 function test_input($data) {
@@ -107,7 +172,7 @@ function test_input($data) {
 
 <?php require_once './header.php'; ?>
 <div id="content">
-    <?php require_once './sidebar.php'; ?>
+<?php require_once './sidebar.php'; ?>
     <div class="contactform">
 
         <p class="txtcontus"><font color="white"><strong>Contact Us</strong></font></p><br />
@@ -115,7 +180,7 @@ function test_input($data) {
             <fieldset class="contusfs" style="width:405px;height: 270px;">
                 <div style="padding:15px 0px 10px 20px">
                     Name<span class="error">*: <input type="text" name="name">
-                        <?php echo $nameErr; ?></span>
+<?php echo $nameErr; ?></span>
                     <br><br>           
                     E-mail<span class="error">*: <input type="text" name="email">
                         <?php echo $emailErr; ?></span>
